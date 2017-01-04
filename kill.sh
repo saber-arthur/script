db4d52f267e502cca0d18f02fde3982fa5853ca0
Chengjie Qin <chengjie.qin@graphsql.com>
2017-01-04 15:54:02 -0800

    make kill not killing grep & kill.sh itself.

--- kill.sh
+++ kill.sh
@@ -6,6 +6,6 @@ then
 	exit
 fi 
 
-pids=`ps aux | grep "$1" | awk '{print $2}' | tr '\n' ' '`
+pids=`ps aux | grep -v "kill" | grep -v "grep" | grep "$1" | awk '{print $2}' | tr '\n' ' '`
 echo "I am going to kill $pids"
 kill $pids
